c56eac2d610fdc7466a4c40f09fa32ebd8003748
Elizabeth Avelar Mercado <elizabethavelarmercado@airbears2-10-142-132-143.airbears2.1918.berkeley.edu>
2018-10-08 17:16:48 -0700

    made some changes

--- testing.go
+++ testing.go
@@ -4,6 +4,7 @@ import (
 	"github.com/nweaver/cs161-p2/userlib"
 
 	"encoding/json"
+	"encoding/hex"
 
 	//"github.com/google/uuid"
 
@@ -23,7 +24,11 @@ type User struct {
 var datastore = make(map[string][]byte)
 
 func main() {
+	test_init_user()
 
+} 
+
+func  test_init_user() {
 	password := "eliavelar"
 	username := "computerscienceEECS"
 
@@ -42,26 +47,47 @@ func main() {
 
 	// Marshall object then encrypt 
 	msg, _ := json.Marshal(user_init)
-	//fmt.Println(mar);
+	fmt.Println("---------------------This is the length of unecrypted user struct -------------------------------")
+	EncodeStringMarshal_user := hex.EncodeToString(msg)
+	// fmt.Println(len(EncodeStringMarshal_user))
+
+	// fmt.Println("---------------------This is the unecrypted user struct -------------------------------")
+	// fmt.Println(EncodeStringMarshal_user);
+
+	// fmt.Println("---------------------This is the Encrypted user struct -------------------------------")
+	 Encrypted_User := cfb_encrypt(Kgen, msg, IV) 
+	// encodedStr := hex.EncodeToString(Encrypted_User)
+	// fmt.Println(encodedStr)
+
+	// fmt.Println("---------------------This is the decrypted user struct -------------------------------")
+	// //decrypted_User := cfb_decrypt(Kgen, Encrypted_User, IV) 
+    Decrypted_user := cfb_decrypt(Kgen, Encrypted_User, IV) 
+    EncodeDecryotedStr := hex.EncodeToString(Decrypted_user)
+
+	//fmt.Println(EncodeDecryotedStr)
+
+    // Print if EncodedString and Decoded String are the sames
+	fmt.Println(EncodeStringMarshal_user == EncodeDecryotedStr)
 
 
-	//Encrypted_User := userlib.CFBEncrypter(Kgen, IV)
-	Encrypted_User := cfb_encrypt(Kgen, msg, IV) 
-	encrypted_PlusIV := append(IV, Encrypted_User...)
+
+//encrypted_PlusIV := append(IV, Encrypted_User...)
 	//s := string(signature)
 	//user_data_store = "signatures_" + s
 	
 
 
-	fmt.Println("this is IV")
-	fmt.Println(IV)
+	//fmt.Println("this is IV")
+	//fmt.Println(IV)
 	
 	// fmt.Println("this is the original message")
 	// fmt.Println(msg)
 
 	// fmt.Println("this is the encypted Message")
-	fmt.Println(Encrypted_User)
+	
 
+
+//Writting the hash of user into the data store
 	// fmt.Println("after appending")
 	// s := string(signature[:])
 	// user_data_store := "signatures_" + s
@@ -71,18 +97,20 @@ func main() {
 	// datastore[user_data_store] = expectedMAC
 	// fmt.Println(expectedMAC)
 	// fmt.Println(user_data_store)
-	sha256 := userlib.NewSHA256()
-	sha256.Write([]byte(Kgen))
-	user_lookup_id := "users_" + string(sha256.Sum(nil))
-	datastore[user_lookup_id] = encrypted_PlusIV
 
-	fmt.Println("this is the user_lookup_id")
 
-	fmt.Println(user_lookup_id)
+// Writting user into 
+	// sha256 := userlib.NewSHA256()
+	// sha256.Write([]byte(Kgen))
+	// user_lookup_id := "users_" + string(sha256.Sum(nil))
+	// datastore[user_lookup_id] = encrypted_PlusIV
 
+	// fmt.Println("this is the user_lookup_id")
+
+	// fmt.Println(user_lookup_id)
 
 	
-} 
+}
 
 func cfb_encrypt(key []byte,  plainText []byte, iv []byte) (cipherText []byte) {
 	stream := userlib.CFBEncrypter(key, iv)
@@ -90,6 +118,16 @@ func cfb_encrypt(key []byte,  plainText []byte, iv []byte) (cipherText []byte) {
 
 	//stream.XORKeyStream(ciphertext[aes.BlockSize:], plaintext)
 	stream.XORKeyStream(cipherText, plainText)
+	return 
+	
+}
+
+func cfb_decrypt(key []byte,  ciphertext []byte, iv []byte) (plaintext []byte){
+	stream := userlib.CFBDecrypter(key, iv)
+	plaintext = make([]byte, len(ciphertext))
+	stream.XORKeyStream(plaintext, ciphertext)
+	//fmt.Println(ciphertext)
+	//plaintext = ciphertext
 	return 
 	
 }
